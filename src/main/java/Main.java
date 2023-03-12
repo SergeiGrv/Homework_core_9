a09ce22b9fa5e062cdfdb1d0415ebce39c10d97d
Sergei Gridnev <sergeigr32@gmail.com>
2023-03-13 00:46:12 +0300

    "Initial commit"

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static final String ULR_CATS = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(ULR_CATS);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        //Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        List<Cats> cats = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+        });
+        cats.stream().filter(s -> s.getUpvotes() != 0).toList().forEach(System.out::println);
+    }
+}
